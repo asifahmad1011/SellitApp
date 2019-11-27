0afc62752310d7d6b6e62bf988813cdf318c30f5
ParvinAkhundov <pakhundof@gmail.com>
2019-11-27 22:51:01 +0100

    config file for db, connection APi,Model Product, Controller Product

---
+++ api-services/model/ProductModel.js
@@ -0,0 +1,100 @@
+/*user model*/
+var Sequelize = require('sequelize');
+var sequelize = require('../common/mysql');
+
+var Product = sequelize.define('products', {
+	product_id: {
+		type: Sequelize.BIGINT(5),
+		autoIncrement: true,
+		primaryKey: true,
+		unique: true,
+		allowNull: false
+	},
+	product_name: {
+		type: Sequelize.STRING(1000),
+		allowNull: false
+	},
+	// slug: {
+	// 	type: Sequelize.STRING(25)
+	// },
+	description: {
+		type: Sequelize.TEXT(1000)
+	},
+	price: {
+		type: Sequelize.BIGINT(11),
+		allowNull: false
+	},
+	// img_id: {
+	// 	type: Sequelize.BIGINT(11)
+	// },
+	image: {
+		type: Sequelize.STRING(1000)
+	},
+	// seller_id: {
+	// 	type: Sequelize.BIGINT(11)
+	// }
+	// ,
+	// more_details: {
+	// 	type: Sequelize.STRING(145)
+	// },
+	// status: {
+	// 	type: Sequelize.BIGINT(1)
+	// }
+	// ,
+	// category_id: {
+	// 	type: Sequelize.BIGINT(11)
+	// },
+	// condition: {
+	// 	type: Sequelize.BIGINT(11)
+	// },
+	// create_date: {
+	// 	type: Sequelize.DATE
+	// },
+	// modified_date: {
+	// 	type: Sequelize.DATE
+	// }
+	year: {
+		type: Sequelize.STRING(20)
+	}
+
+});
+
+module.exports.getAllProducts = function(callback) {
+        Product.findAll()
+		.then(function(related) {
+			//console.log(related[0].role.role);
+			callback( related);
+		  })
+		  .catch(function(err) {
+			//console.log(err);
+			callback(err);
+		  });
+	}
+
+	module.exports.getAllProductsByName = function(product_name,callback) {
+        Product.findAll({
+			where: {
+				 	  name:product_name
+				 	}			
+		  })
+		.then(function(related) {
+			//console.log(related[0].role.role);
+			callback( related);
+		  })
+		  .catch(function(err) {
+			//console.log(err);
+			callback(err);
+		  });
+	}
+
+	// models.product.findAll({
+	// 	where: {
+	// 	  category: category
+	// 	},
+	// 	order: [
+	// 		['views', 'DESC']
+	// 	],
+	// 	include: [
+	// 	  {model: models.image, as: 'images'}
+	// 	]
+	//   })
---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {
+	port: 8080,
+	mysql: {
+		host: '18.224.199.153',
+    	user: 'remote',
+    	password: 'Remote2019@',
+		database: 'dummy_products',
+		port: 3306
+	}
+}
+
+module.exports = config

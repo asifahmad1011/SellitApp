9c3fdc4172ad91c6ea7a6ae1aa8f99c0079e0c72
gitdamilare <57131003+gitdamilare@users.noreply.github.com>
2019-11-30 12:31:42 +0100

    Installed a missing dependency
    1. Installed a missing module
    'ngx-pagination'
    2. Updated the Host IP Address from 18.224.199.153 to 3.134.103.215

--- api-services/config.js
+++ api-services/config.js
@@ -1,7 +1,8 @@
 var config = {
 	port: 8080,
 	mysql: {
-		host: '18.224.199.153',
+    /*host: '18.224.199.153',*/
+      host: '3.134.103.215',
     	user: 'remote',
     	password: 'Remote2019@',
 		database: 'dummy_products',

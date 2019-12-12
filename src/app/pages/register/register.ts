c89ed1443356aab0ea15746d42ade976ac6e977a
asifahmad1011 <asif.apag@gmail.com>
2019-12-12 17:35:59 +0100

    feat: user registration wo validation.

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,6 +1,16 @@
-export class Register {
-    first_name:string;  
-    last_name:string;  
-    email:string; 
-    password:string;  
-}
+export interface Register {
+    matrikel_number: bigint;
+    first_name: string;
+    last_name: string;
+    dob: Date;
+    email: string;
+    address: string;
+    phone_number: string;
+    postal_code: string;
+    role_id: bigint;
+    username: string;
+    password: string;
+    created_date: Date;
+    modified_date: Date;
+  }
+  

67293c2a932896fb87c03a4cba500ac94198a2ac
damiie <>
2019-12-16 00:39:37 +0100

    Created Dashboard Services
    Updated Swagger with Dashboard Services

--- api-services/routes/Dashboard.js
+++ api-services/routes/Dashboard.js
@@ -2,29 +2,56 @@ var express = require('express');
 var router = express.Router();
 var Dash = require("../controller/DashboardController");
 
-router.get('/', function(req, res, next) {
-        var totalProductt = 0;
-        Dash.totalProduct((rows) => {
-            if (!rows) {
-              console.log(rows);
+router.get('/', function (req, res, next) {
+  var totalProduct = 0;
+  var totalSoldProduct = 0;
+  var totalPendingProduct = 0;
+  var totalApprovedProduct = 0;
+  var totalInActiveProduct = 0;
+  var totalDeletedProduct = 0;
+  Dash.totalProduct((rows) => {
+    if (!rows) {
+      console.log(rows);
+      res.json({
+        "status": "failed",
+        "cahat": null
+      })
+    } else {
+      totalProduct = rows;
+      Dash.totalSoldProduct((rows) => {
+        totalSoldProduct = rows;
+        Dash.totalPendingProduct((rows) => {
+          totalPendingProduct = rows;
+          Dash.totalApprovedProduct((rows) => {
+            totalApprovedProduct = rows;
+            Dash.totalInActiveProduct((rows) => {
+              totalInActiveProduct = rows;
+              Dash.totalDeletedProduct((rows) => {
+                totalDeletedProduct = rows;
                 res.json({
-                    "status": "failed",
-                    "cahat": null
+                  "totalProduct": totalProduct,
+                  "totalSoldProduct": totalSoldProduct,
+                  "totalPendingProduct": totalPendingProduct,
+                  "totalApprovedProduct" : totalApprovedProduct,
+                  "totalInActiveProduct" :totalInActiveProduct,
+                  "totalDeletedProduct" :  totalDeletedProduct,
                 })
-            } else {
-             // console.log(rows);
-              totalProductt = rows;
-              //getDashboardResult();
-            }
+              })
+            })
+          })
         });
-        console.log(39);
-        res.json({totalProductt});
-    });
+      })
 
-    function getDashboardResult(){
-      var totalProduct = Dash.totalProduct((rows) => { rows});
-      console.log(totalProduct + "from getDashboardResult");
+
     }
+  });
+
+});
+
+function getDashboardResult() {
+  var totalProduct = Dash.totalProduct((rows) => { rows });
+  console.log(totalProduct + "from getDashboardResult");
+}
 
 
-  module.exports = router;
+module.exports = router;
